1e84211b9bcaa2eb1bd516f1adb4985838e95259
Deyvi Edwin Jara García <djaravirtual@gmail.com>
2021-12-22 18:48:42 -0500

    Reto #1: navbar and modal disappear with scroll

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 const useGetProducts = (API_URL) => {
   const [products, setProducts] = useState([]);
   useEffect(async () => {
-    const response = await fetch(`${API_URL}/products?limit=5&offset=5`).then(
+    const response = await fetch(`${API_URL}/products?limit=50&offset=50`).then(
       (res) => res.json()
     );
     setProducts(response);
